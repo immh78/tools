bfcd3bccf274c1cc877f832101cf9986bdd9712c
immh78 <85915971+immh78@users.noreply.github.com>
2025-05-13 11:26:56 +0900

    vuetify 설정

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
-import './style.css'
+//import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+
+// Vuetify 관련 import
+import "vuetify/styles";
+import { createVuetify } from 'vuetify';
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+// Vuetify 인스턴스 생성
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(vuetify).mount('#app');
+
