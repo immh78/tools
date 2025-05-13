558a3b6c0aae41b913140b6207f65cfe62e314fc
immh78 <85915971+immh78@users.noreply.github.com>
2025-05-13 19:04:34 +0900

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted } from "vue";
 
-const chapters = ref(["Day 6"]);
+const chapters = ref(["Day 6", "Day 7"]);
 
 const toggleMode = ref("quiz");
 const toggleChapter = ref("");
@@ -26,6 +26,22 @@ function selectingWord(param) {
   totalCount.value = selectWords.value.length;  
 }
 
+function changeMode() {
+  initValue();
+  
+  if (toggleMode.value == "quiz") {
+    toggleChapter.value = "";
+    selectingWord('');
+    pickRandomWord();
+  }
+}
+
+function changeChapter(param) {
+  initValue();
+  selectingWord(param);
+  pickRandomWord();
+}
+
 function pickRandomWord() {
   if (selectWords.value.length === 0) {
     currentWord.value.word = "완료!";
@@ -47,7 +63,7 @@ function markCorrect() {
   correctCount.value ++;
   selectWords.value = selectWords.value.filter(item => item.word !== currentWord.value.word);
 
-  console.log("count : ", selectWords.value.length);
+  //console.log("count : ", selectWords.value.length);
   pickRandomWord();
   updateProgress();
 }
@@ -59,7 +75,7 @@ function markWrong() {
   firstElement.wrongCount += 1;
   selectWords.value.push(firstElement);
 
-  console.log("count : ", selectWords.value.length);
+  //console.log("count : ", selectWords.value.length);
 
   pickRandomWord();
 
@@ -74,7 +90,16 @@ function speechWord() {
   const utterance = new SpeechSynthesisUtterance(currentWord.value.word);
   utterance.lang = 'en-US';
   speechSynthesis.speak(utterance);
-  console.log("speech : ", currentWord.value.word);
+  //console.log("speech : ", currentWord.value.word);
+}
+
+function initValue() {
+  correctCount.value = 0;
+  wrongCount.value = 0;
+  currentWord.value.word = "";
+  currentWord.value.meaning = "";
+  currentWord.value.wrongCount = 0;
+  progress.value = 0;  
 }
 
 
@@ -99,13 +124,13 @@ onMounted(() => {
       <v-container>
         <v-row justify="center">
           <v-col cols="auto">
-            <v-btn-toggle v-model="toggleMode" color="primary" mandatory>
-              <v-btn value="quiz" @click="selectingWord('')">
-                <span class="hidden-sm-and-down">퀴즈</span>
+            <v-btn-toggle v-model="toggleMode" variant="outlined" color="primary" mandatory>
+              <v-btn value="quiz" @click="changeMode()">
+                <span>퀴즈</span>
                 <v-icon end>mdi-chat-question</v-icon>
               </v-btn>
-              <v-btn value="memorize">
-                <span class="hidden-sm-and-down">암기</span>
+              <v-btn value="memorize" @click="changeMode()">
+                <span>암기</span>
                 <v-icon end>mdi-head-question</v-icon>
               </v-btn>
             </v-btn-toggle>
@@ -114,7 +139,7 @@ onMounted(() => {
         <v-row justify="center" v-if="toggleMode === 'memorize'">
           <v-col cols="auto">
             <v-btn-toggle v-model="toggleChapter" color="deep-purple-accent-3" rounded="0" group>
-              <v-btn v-for="chapter in chapters" :key="chapter" :value="chapter" @click="selectingWord(chapter)">
+              <v-btn v-for="chapter in chapters" :key="chapter" :value="chapter" @click="changeChapter(chapter)">
                 {{ chapter }}
               </v-btn>
 
@@ -124,8 +149,8 @@ onMounted(() => {
         <v-row justify="center">
           <v-col cols="auto">
             <span id="word" :style="{fontSize: wordFontSize + 'px'}" @click="speechWord()">{{ currentWord.word }}</span>
-            <span id="wrong" style="fontSize: 8px">
-              <v-icon v-for="n in currentWord.wrongCount">mdi-close-thick</v-icon>
+            <span id="wrong">
+              <v-icon color="red-darken-4" v-for="n in currentWord.wrongCount">mdi-close-thick</v-icon>
             </span>
           </v-col>
         </v-row>
@@ -136,23 +161,30 @@ onMounted(() => {
         </v-row>
         <v-row justify="center">
           <v-col cols="auto">
-            <v-btn @click="isMeaningView = !isMeaningView"><v-icon>mdi-magnify</v-icon>뜯보기</v-btn>
+            <v-btn color="light-green-lighten-5" @click="isMeaningView = !isMeaningView"><v-icon>mdi-magnify</v-icon>뜯보기</v-btn>
           </v-col>
           <v-col cols="auto">
-            <v-btn @click="markCorrect()"><v-icon>mdi-check-bold</v-icon>정답</v-btn>
+            <v-badge color="blue" :content="correctCount"><v-btn color="blue-lighten-5" @click="markCorrect()"><v-icon>mdi-check-bold</v-icon>정답</v-btn></v-badge>
           </v-col>
           <v-col cols="auto">
-            <v-btn @click="markWrong()"><v-icon>mdi-close-thick</v-icon>오답</v-btn>
+            <v-badge color="error" :content="wrongCount"><v-btn color="red-lighten-5" @click="markWrong()"><v-icon>mdi-close-thick</v-icon>오답</v-btn></v-badge>
           </v-col>
 
         </v-row>
       </v-container>
 
     </v-main>
-    <v-progress-linear :model-value="progress" ></v-progress-linear>
-    <v-footer><v-icon>mdi-check-bold</v-icon>{{ correctCount }}  <v-icon>mdi-close-thick</v-icon>{{ wrongCount }}</v-footer>
+    <v-progress-linear :model-value="progress" ></v-progress-linear>    
   </v-app>
 
 </template>
 
-<style scoped></style>
+<style scoped>
+  #wrong {
+    font-size: 8px;
+  }
+
+  #meaning {
+    color: #00695C;
+  }
+</style>
