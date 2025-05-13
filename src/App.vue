45d763909d99e13345a9fb42ece141eec10a85af
immh78 <85915971+immh78@users.noreply.github.com>
2025-05-13 11:34:57 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref } from "vue";
+
+  function sayHello() {
+    alert("Good!!!");
+  }
 </script>
 
 <template>
-  <div><H3>TEST1234</H3></div>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<v-app>
+  <v-main>
+    <v-container class="my-5">
+      <v-btn color="primary" @click="sayHello">Test</v-btn>
+    </v-container>
+  </v-main>
+</v-app>
   
 </template>
 
