fc3830e3c00b8b5723bf5bb3ff0f8f0c77a6adf0
immh78 <85915971+immh78@users.noreply.github.com>
2025-05-16 11:43:03 +0900

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,46 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dotenv from 'dotenv';
+import { VitePWA } from 'vite-plugin-pwa'
 
 // 현재 NODE_ENV에 따라 올바른 .env 파일 로드
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate', // service worker가 변경되면 자동으로 업데이트
+      includeAssets: ['book_6.svg', 'book_6.png'], // 정적 파일
+      manifest: {
+        name: '채원이 영단어 암기',
+        short_name: 'cwEngWord',
+        description: '채원이 영단어 암기용 앱',
+        theme_color: '#42b883',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'book_6.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'book_6.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'book_6.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   base: '/vite-project/', 
   define: {
     // Vite에서는 process.env 대신 import.meta.env 사용이 권장됩니다
