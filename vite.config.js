c23cafa0995c8d6ac2ef521d56a7ea20a748c0d7
immh78 <85915971+immh78@users.noreply.github.com>
2025-05-17 23:24:44 +0900

    ....

--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,46 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dotenv from 'dotenv';
-import { VitePWA } from 'vite-plugin-pwa'
+//import { VitePWA } from 'vite-plugin-pwa'
 
 // 현재 NODE_ENV에 따라 올바른 .env 파일 로드
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 export default defineConfig({
   plugins: [
-    vue(),
-    VitePWA({
-      registerType: 'autoUpdate', // service worker가 변경되면 자동으로 업데이트
-      includeAssets: ['book_6.svg', 'book_6.png'], // 정적 파일
-      manifest: {
-        name: '채원이 영단어 암기',
-        short_name: 'cwEngWord',
-        description: '채원이 영단어 암기용 앱',
-        theme_color: '#42b883',
-        background_color: '#ffffff',
-        display: 'standalone',
-        scope: '/vite-project/',        
-        start_url: '/vite-project/#/eng-quiz',
-        icons: [
-          {
-            src: 'book_6.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'book_6.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'book_6.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
+    vue()
+    // VitePWA({
+    //   registerType: 'autoUpdate', // service worker가 변경되면 자동으로 업데이트
+    //   includeAssets: ['book_6.svg', 'book_6.png'], // 정적 파일
+    //   manifest: {
+    //     name: '채원이 영단어 암기',
+    //     short_name: 'cwEngWord',
+    //     description: '채원이 영단어 암기용 앱',
+    //     theme_color: '#42b883',
+    //     background_color: '#ffffff',
+    //     display: 'standalone',
+        //scope: '/vite-project/',        
+        //start_url: '/vite-project/#/eng-quiz',
+        // icons: [
+        //   {
+        //     src: 'book_6.png',
+        //     sizes: '192x192',
+        //     type: 'image/png'
+        //   },
+        //   {
+        //     src: 'book_6.png',
+        //     sizes: '512x512',
+        //     type: 'image/png'
+        //   },
+        //   {
+        //     src: 'book_6.png',
+        //     sizes: '512x512',
+        //     type: 'image/png',
+        //     purpose: 'any maskable'
+        //   }
+        // ]
+    //   }
+    // })
   ],
   base: '/vite-project/', 
   define: {
