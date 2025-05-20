e45eb454d74596beaf540d5704cc083faf5e30da
immh78 <85915971+immh78@users.noreply.github.com>
2025-05-20 17:43:56 +0900

    quiz user누락 일괄 보정

---
+++ src/pages/EngQuizChapterAdj.vue
@@ -0,0 +1,90 @@
+<script setup>
+import { database, ref as firebaseRef, get, update } from "../config/firebase";
+import { ref, onMounted } from 'vue';
+
+const quizChapters = ref(null);
+const chapters = ref(null);
+const error = ref(null);
+
+async function fetchquizChapters() {
+    const dbRef = firebaseRef(database, "eng-quiz-chapter");
+    await get(dbRef)
+        .then(snapshot => {
+            if (snapshot.exists()) {
+                quizChapters.value = snapshot.val();
+            } else {
+                console.log("No data available");
+            }
+        })
+        .catch(err => {
+            console.error("Error fetching data:", err);
+            error.value = err.message;
+        });
+}
+
+async function getChapter() {
+    await fetchquizChapters();
+
+    //console.log(user);
+
+    if (quizChapters.value) {
+        chapters.value = Object.values(quizChapters.value)
+            .filter(item => !item.hasOwnProperty('user')) // select가 true인 항목 필터링
+            .map(item => ({
+                chapter: item.chapter,
+                select: item.select
+            }));
+    }
+
+    console.log(chapters.value);
+}
+
+async function saveQuizChapter(data) {
+    try {
+        const dbRef = firebaseRef(database, "eng-quiz-chapter");
+        await update(dbRef, data); // 데이터를 저장
+        console.log("Data saved successfully!");
+    } catch (err) {
+        console.error("Error saving data:", err);
+    }
+}
+
+
+function setChapter() {
+
+    console.log("setChapter!!");
+
+    let user = "";
+    let idx = -1;
+
+    console.log(chapters.value);
+
+    for (const row of chapters.value) {
+        if (row.chapter.length == 2) {
+            user = "GW";
+        } else {
+            user = "CW";
+        }
+
+        console.log(user);
+
+        idx = Object.values(quizChapters.value).findIndex(item => item.chapter === row.chapter);
+
+        console.log("idx :", idx);
+
+        if (idx >= 0 ) {
+            const saveData = { [idx]: { "chapter": row.chapter, "select": row.select, "user": user }};
+            saveQuizChapter(saveData);
+        }
+    }
+}
+
+onMounted(async () => {
+    console.log("onMounted!!");
+});
+</script>
+
+<template>
+    <v-btn @click="getChapter()">데이터 오류 조회</v-btn>
+    <v-btn @click="setChapter()">데이터 오류 보정</v-btn>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 // src/router/index.js 또는 main.js
-import { createRouter, createWebHashHistory } from 'vue-router'
-import DuesList from '../pages/DuesList.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import DuesList from '../pages/DuesList.vue';
+import EnqQuizChapterAdj from '../pages/EngQuizChapterAdj.vue'
 
 const routes = [
   { path: '/', redirect: '/dues-list' },
-  { path: '/dues-list', component: DuesList }
+  { path: '/dues-list', component: DuesList },
+  { path: '/engquiz-adj', component: EnqQuizChapterAdj },
 ]
 
 const router = createRouter({
