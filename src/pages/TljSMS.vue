1765bd13c58c27da4cc41c5039bb3c3bcf4e6b60
immh78 <85915971+immh78@users.noreply.github.com>
2025-05-23 11:37:07 +0900

    sms(3)

--- src/pages/TljSMS.vue
+++ src/pages/TljSMS.vue
@@ -8,6 +8,11 @@ const API_KEY = 'AIzaSyAjcEIdV46fa6Kw3Hdyzf3No_3cXtScRLc'; // 본인의 Google A
 const SHEET_ID = '1z9zfiUomAKR99RLNblL4melEgFexRIGdYgbewhTIB38'; // 본인의 Sheet ID 입력
 const RANGE = '뚜레쥬르'; // 원하는 범위 지정
 
+const headers = ref([
+    { title: '날짜', align: 'start', key: 'date', value: 'date', width: 110 },
+    { title: '금액', align: 'end', sortable: false, key: 'amount', value: 'amount' },
+]);
+
 const phoneNumber = ref('01092751025')
 
 function sendSMS() {
@@ -16,9 +21,10 @@ function sendSMS() {
 }
 
 function formatSMSMessage() {
-    rows.value.splice(rows.value.length - 1, 0, { date: "--------------", amount: "" });
+    const msgRows = [...rows.value];
+    msgRows.splice(msgRows.length - 1, 0, { date: "--------------------", amount: "" });
 
-    msg.value = rows.value
+    msg.value = msgRows
         .map(row => `${row.date}     \t${row.amount}${row.date.substring(0,1) === "-" ? "":"원"}`) // 각 행을 "날짜\t금액원" 형식으로 변환
         .join('\n'); // 각 행을 줄바꿈으로 연결
 }
@@ -50,7 +56,29 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div>
-        <button @click="sendSMS">SMS 보내기</button>
-    </div>
+    <v-app>
+        <v-main>
+            <v-app-bar color="teal-darken-4"> <!--image="https://picsum.photos/1920/1080?random-->
+                <template v-slot:image>
+                    <v-img gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"></v-img>
+                </template>
+                <template v-slot:append>
+                    <v-btn icon="mdi-send" @click="sendSMS()"></v-btn>
+                </template>
+                <v-app-bar-title><v-icon>mdi-cupcake</v-icon> 뚜레쥬르 선결제 금액</v-app-bar-title>
+            </v-app-bar>
+
+            <v-data-table :headers="headers" :items="rows" class="elevation-1" no-data-text="조회중입니다."
+                hide-default-footer items-per-page="-1" :show-items-per-page="false">               
+
+                <!-- 페이지네이션 버튼 중앙 정렬 -->
+                <template v-slot:footer.page-text>
+                    <div class="text-center">
+                        <v-pagination v-model="page" :length="Math.ceil(rows.length / 14)"
+                            total-visible="7"></v-pagination>
+                    </div>
+                </template>
+            </v-data-table>            
+        </v-main>
+    </v-app>
 </template>
