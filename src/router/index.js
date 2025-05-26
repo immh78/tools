840697b62426c3707494ed2af0dc11b3dc1f06f9
immh78 <85915971+immh78@users.noreply.github.com>
2025-05-26 09:13:14 +0900

    route 텍스트 수정

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import TljSMS from '../pages/TljSMS.vue'
 
 const routes = [
   { path: '/', component: Main, comment: '메인 페이지' },
-  { path: '/dues-list', component: DuesList, comment: '우리린 가족여행 회비 목록' },
+  { path: '/dues-list', component: DuesList, comment: '우미린 가족여행 회비 목록' },
   { path: '/engquiz-adj', component: EnqQuizChapterAdj, comment: '영어 퀴즈 DB 오류 조정' },
   { path: '/tlj-sms', component: TljSMS, comment: '뚜레쥬르 선입금 문자발송' },
 ]
